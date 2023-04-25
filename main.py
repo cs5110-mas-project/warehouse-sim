d0f12f932c3a7d788cba9e1c0e2b77f523b76cbe
jfitzusu <A02261889@usu.edu>
2023-04-24 20:36:16 -0600

    Implemented Coop, Fake Coop, Agressive, and Optimal(DO NOT RUN) Solutions

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ def main():
     # TODO add argument for selecting which factory to use, add functionality to WarhouseSimulator class
     parser = argparse.ArgumentParser()
     parser.add_argument("-fps", "--frames-per-sec", type=int, default=5, help="How many times per second the simulator runs at")
-    parser.add_argument("-c", "--competitive", action="store_true", help="Whether to run the competitive simulation, default is cooperative")
+    parser.add_argument("-m", "--mode", type=str, choices=['a', 'b', 'c', 'd', 'e'], default='a', help="Simulation mode. a-Agressive, b-Competative, c-Cooperative, d-Bad Actor, e-Fully Managed")
     parser.add_argument("-ng", "--no-gui", action="store_false", default=True, help="Whether to run with the GUI or not, default is true")
-    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Prints extra info during simulation")
+    parser.add_argument("-v", "--verbose", action="store_true", default=True, help="Prints extra info during simulation")
     parser.add_argument("-i", "--iterations", type=int, default=1, help="Number of times to run the simulation")
     parser.add_argument("-w", "--warehouse", type=int, default=0, help="Integer for which warehouse to run the simulator with (0-2)")
     args = parser.parse_args()
     random.seed(1337)
-    simulation = warehouseSimulator.WarehouseSimulator(args.frames_per_sec, args.competitive, args.no_gui, args.verbose, args.iterations, constants.warerhouses[args.warehouse])
+
+    simulation = warehouseSimulator.WarehouseSimulator(args.frames_per_sec, args.mode, args.no_gui, args.verbose, args.iterations, constants.warerhouses[args.warehouse])
     simulation.run()
 
 
--- warehouseSimulator.py
+++ warehouseSimulator.py
@@ -19,18 +19,21 @@ class Job:
     endY: int
     activationTime: int
     assigned: bool
+    fake: bool
 
+    def __hash__(self):
+        return hash(self.startX) * 7 + hash(self.startY) * 13 + 7 * hash(self.fake)
 class WarehouseSimulator:
 
     
-    def __init__(self, fps, competitive, gui, verbose, iterations, warehouse) -> None:
+    def __init__(self, fps, mode, gui, verbose, iterations, warehouse) -> None:
         # TODO add functionality for competitive and cooperative
         self.gui = gui
         self.fps = fps
         self.verbose = verbose
         self.iterations = iterations
         self.cell_size = 15
-        self.competitive = competitive
+        self.mode = mode
         self.warehouse = warehouse
         self.num_horizontal_cells = len(self.warehouse[0])
         self.num_vertical_cells = len(self.warehouse)
@@ -51,7 +54,7 @@ class WarehouseSimulator:
             self.screen = pygame.display.set_mode((self.window_width, self.window_height))
             # self.font = pygame.font.Font('freesansbold.ttf', 18)
             self.drawManager = DrawManager(self.screen, self.window_width, self.window_height, self.cell_size, self.warehouse)
-        self.warehouseManager = WarehouseManager(self.verbose)
+        self.warehouseManager = WarehouseManager(mode, self.verbose)
 
 
 
@@ -74,7 +77,7 @@ class WarehouseSimulator:
             if i > 0 and i % numJobsAssignedAtATime == 0:
                 activationTime += 30
             jobList.append(Job(start.location[0], start.location[1],
-                        end.location[0], end.location[1], activationTime, False))
+                        end.location[0], end.location[1], activationTime, False, False))
 
         return jobList
 
@@ -110,7 +113,7 @@ class WarehouseSimulator:
                 if event.type == pygame.QUIT:
                     return False
         
-            
+
         self.warehouseManager.update(self.robots, self.jobList, totalTicks)
 
         # There is a chance that all the jobs have been completed before the next round of jobs get assigned.
@@ -140,6 +143,7 @@ class WarehouseSimulator:
             keepGoing = True
             while keepGoing:   
                 keepGoing = self.update(totalTicks)
+                self.stats.ticks += 1
                 totalTicks += 1
 
             self.stats.printReport()
